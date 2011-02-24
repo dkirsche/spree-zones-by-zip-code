6bf73621890db2f076cc6d3ba45a83801e1144c0
Gagan Awhad <regist.gagan@gmail.com>
2011-02-24 13:30:51 -0600

    Added some flesh to the ZipCode model. Allowing each zip code to be in multiple zones. Added the two other what-seem-to-be-as-standard methods to the model

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,3 +1,14 @@
 class ZipCode < ActiveRecord::Base
+  :has_many :zone_members
+  :has_many :zones, :through => :zone_member
+
+  validates :value, :presence => true
+
+  def <=>(other)
+    value <=> other.value
+  end
+  def to_s
+    value
+  end
 end
 
