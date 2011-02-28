72db070d47b32b62dfc012fddf190cd133160a33
Gagan Awhad <regist.gagan@gmail.com>
2011-02-28 13:49:10 -0600

    Added code to zip codes from the spree code for states controller for the admin area. It can be uncommented as needed

--- app/controllers/admin/zip_codes_controller.rb
+++ app/controllers/admin/zip_codes_controller.rb
@@ -1,7 +1,30 @@
 class Admin::ZipCodesController < Admin::BaseController
   resource_controller
-#  def index
-#    @zip_codes = ZipCode.all
-#  end
-end
+  new_action.response do |wants|
+    wants.html {render :layout => !request.xhr?}
+  end
+=begin
+  before_filter :load_data
 
+  index.response do |wants|
+    wants.html
+    wants.js do
+      render :partial => 'state_list.html.erb'
+    end
+  end
+
+
+  create.wants.html { redirect_to admin_country_states_url(@country) }
+  update.wants.html { redirect_to admin_country_states_url(@country) }
+
+  private
+
+    def collection
+      @collection ||= end_of_association_chain.order('name')
+    end
+
+    def load_data
+      @countries = Country.order('name')
+    end
+=end
+end
